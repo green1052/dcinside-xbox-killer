a9e6c780b4c3eeaa66b4d9d6aa9a85b2539b1f9a
green1052 <cydyellowgreen@gmail.com>
2020-11-03 19:17:49 +0900

    Update dcinside-xbox-killer.user.js

--- dcinside-xbox-killer.user.js
+++ dcinside-xbox-killer.user.js
@@ -1,27 +1,22 @@
 // ==UserScript==
-// @name		dcinside link fix
-// @namespace	https://github.com/green1052
-// @version		1.0.0
-// @homepageURL	https://github.com/green1052/dcinside-link-fix
-// @downloadURL https://raw.githubusercontent.com/green1052/dcinside-link-fix/main/dcinside-link-fix.js
-// @author		green1052
-// @description	디시인사이드 본문 내용에 링크가 있을 경우 a href로 바꿔줍니다.
-// @include     *.dcinside.com/*
-// @run-at		document-end
+// @name         dcinside xbox killer
+// @namespace	 https://github.com/green1052
+// @homepageURL	 https://github.com/green1052/dcinside-xbox-killer
+// @downloadURL	 https://raw.githubusercontent.com/green1052/dcinside-xbox-killer/main/dcinside-xbox-killer.user.js
+// @version      1.0.0
+// @description  디시인사이드 엑스 박스 완화 유저 스크립트
+// @author       green1052
+// @include      *.dcinside.com/*
+// @run-at       document-end
 // ==/UserScript==
 
 (function () {
     "use strict";
 
-    const regex = /(http|https):\/\/(\w+:?\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@\-\/]))?/;
+    const imgTag = document.getElementsByTagName("img");
 
-    document.querySelectorAll(".writing_view_box > div > div").forEach(query => {
-        const text = regex.exec(query.textContent)[0];
-        query.innerHTML = query.innerHTML.replace(text, `<a href="${text}" target="_blank">${text}</a>`)
-    });
-
-    document.querySelectorAll(".writing_view_box > div > p").forEach(query => {
-        const text = regex.exec(query.textContent)[0];
-        query.innerHTML = query.innerHTML.replace(text, `<a href="${text}" target="_blank">${text}</a>`)
-    });
+    for (let i = 0; i < imgTag.length; i++) {
+        if (imgTag[i].src.includes(".dcinside.co.kr/viewimage.php?"))
+            imgTag[i].src = `https://image.dcinside.com/viewimage.php?${imgTag[i].src.split("?")[1]}`;
+    }
 })();
